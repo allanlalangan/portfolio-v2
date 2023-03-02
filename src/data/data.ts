455380077a6d07041e3981d9313d52542001d86e
allanlalangan <allanlalangan@gmail.com>
2023-03-02 10:53:50 -0800

    add project img data

--- src/data/data.ts
+++ src/data/data.ts
@@ -8,16 +8,26 @@ type DemoCredentials = {
 	password: string;
 };
 
+type ProjectImage = {
+	src: string;
+	alt: string;
+};
+
 type Project = {
 	title: string;
 	description: string;
 	tech: string[];
-	image: string;
+	image: ProjectImage;
 	repo: string;
 	url: string;
 	complete: boolean;
 	demo?: DemoCredentials;
 };
+
+import nomad from '../../public/assets/nomad-travel.png';
+import projectMe from '../../public/assets/project-me.png';
+import mma from '../public/assets/mma.png';
+import stars from '../public/assets/stars.png';
 
 export const services: Service[] = [
 	{
@@ -57,7 +67,7 @@ export const projects: Project[] = [
 			'web tokens',
 			'rest api',
 		],
-		image: '',
+		image: { src: stars, alt: 'Stars Astrology dashboard page' },
 		repo: 'https://github.com/allanlalangan/stars',
 		url: 'https://stars-astro.netlify.app/',
 		complete: true,
@@ -68,7 +78,7 @@ export const projects: Project[] = [
 		description:
 			'Locate and filter popular restaurants, hotels, and attractions anywhere in the world.',
 		tech: ['react', 'material ui', 'google apis', 'rest api'],
-		image: '',
+		image: { src: nomad, alt: 'Nomad Travel map with marker' },
 		repo: 'https://github.com/allanlalangan/nomad-travel',
 		url: 'https://nononomad.netlify.app/',
 		complete: true,
@@ -78,30 +88,30 @@ export const projects: Project[] = [
 	// 	description:
 	// 		"A responsive landing page design for a local martial arts gym.",
 	// 	tech: ["next", "tailwind css"],
-	// 	image: '',
+	// 	image: { src: mma, alt: "Martial Arts gym landing page" },
 	// 	repo: "https://github.com/allanlalangan/martial-masters-academy",
 	// 	url: "https://mm-acdmy.netlify.app/",
 	// 	complete: true,
 	// },
-	// {
-	// 	title: "project.Me",
-	// 	description: "A fullstack to-do list app. Registered users can save goals.",
-	// 	tech: [
-	// 		"express",
-	// 		"mongodb",
-	// 		"react",
-	// 		"react router",
-	// 		"sass",
-	// 		"redux",
-	// 		"web tokens",
-	// 		"rest api",
-	// 	],
-	// 	image: '',
-	// 	repo: "https://github.com/allanlalangan/dashboard",
-	// 	url: "https://ladev-projectme.netlify.app",
-	// 	complete: true,
-	// 	demo: { email: "demo@demo", password: "demo" },
-	// },
+	{
+		title: 'project.Me',
+		description: 'A fullstack to-do list app. Registered users can save goals.',
+		tech: [
+			'express',
+			'mongodb',
+			'react',
+			'react router',
+			'sass',
+			'redux',
+			'web tokens',
+			'rest api',
+		],
+		image: { src: projectMe, alt: 'Project.me user goals page' },
+		repo: 'https://github.com/allanlalangan/dashboard',
+		url: 'https://ladev-projectme.netlify.app',
+		complete: true,
+		demo: { email: 'demo@demo', password: 'demo' },
+	},
 ];
 
 export {};
