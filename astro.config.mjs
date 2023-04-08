f60c6b01ad0e3fd39ab02c55fac2c591ff931633
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2023-04-07 20:00:30 -0700

    update sanity config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import sanity from 'astro-sanity';
+import sanity from "astro-sanity";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
     sanity({
-      projectId: 'rzka3686',
-      dataset: 'production',
-      apiVersion: '2021-03-25',
-      useCdn: true,
+      projectId: "rzka3686",
+      dataset: "production",
+      apiVersion: "2021-03-25",
+      useCdn: false,
       token: process.env.SANITY_SECRET_TOKEN,
     }),
   ],
